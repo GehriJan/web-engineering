b2d5663f805b65a0e430ed3a1be484dda3510dbd
Jannis Gehring <jannis-gehring@t-online.de>
2023-07-07 14:07:35 +0200

    main site css and Tischtennis-Zauberer Zappelfüßchen

--- views/Spielplan.js
+++ views/Spielplan.js
@@ -1,10 +1,38 @@
 const spielplan_template = /*html*/`
 <template id="TEMPLATE_SPIELPLAN">
-    <Header>
-    <h1>Spielplan</h1>
     <link rel="stylesheet" href="assets/styles/Spielplan.css"/>
     <link rel="stylesheet" href="assets/styles/tournamentPodium.css"/>
-    </Header>
+    
+    <div id="main_img">
+        <img src="./assets/img/pingpong.png">
+    </div>
+    <h2>Turnierbeschreibung</h2>
+    <p>
+        Hallo und herzlich willkommen zu offiziellen Seite des Tischtennis-Grand-Slams 2023. Mit Spannung erwartet freuen wir uns wieder einmal auf schnelle Topspins und gnadenlose Kantenbälle. Am <b>13.07.2024</b> kämpfen die talentiertesten Tischtennisspieler Kontinentaleuropas auf dem prestigeträchtigen <abbr title="Tischtennis-Grand-Slam">TT-GS</abbr> um die Qualifikation für die diesjährige WM. Das Turnier ist wie gewohnt nach dem Brasilianischen K.O.-System aufgebaut.
+        <br/>
+        Um einen fairen Wettkampf zu ermöglichen, lies dir bitte die 
+        <a href="https://www.tischtennis.de/fileadmin/documents/Mein_Sport/Schiedsrichter/Regeln/Internationale_Tischtennisregeln_A_2022-07-04.pdf">aktuellen Regeln der <abbr title="Internationalen Tischtennis Föderation">ITF</abbr></a> durch und mache dich mit den <a href="https://www.nada.de/home">Anti-Doping Regelungen der <abbr title="Nationale Anti-Doping Agentur Deutschland">NADA</abbr> vertraut</a>.
+    </p>
+    <h2>Zeitplan:</h2>
+        <ul>
+            <li>9:00: Akkreditierung durch den Bundestrainer</li>
+            <li>10:00 Beginn der Duelle</li>
+            <li>13:45: Finalblock</li>
+            <li>15:00: Siegerehrung</li>
+        </ul>
+    <h2>Ort</h2>
+        <ul>
+            <li>Scharrena, Fritz-Walter-Weg 5, 70372 Stuttgart</li>
+        </ul>
+    <h2>Mitbringeliste</h2>
+    <ul>
+        <li>Tischtennisschläger (Regelkonform)</li>
+        <li>Sportbekleidung (Regelkonform)</li>
+        <li>Datenschutzbescheinigung:</li>
+        <a href="https://www.tischtennis.de/mein-sport/spielen/wichtigste-spielregeln.html">PDF Download.</a>
+    </ul>
+        
+    <h1>Spielplan</h1>
     <hr>
     <div id="Spielplan">
     <h2>Viertelfinale</h2>
@@ -76,6 +104,28 @@ const spielplan_template = /*html*/`
         <p class="podiumsplatz" id="dritterPlatz">🥉</p>
     </div>
 </div>
+
+<style>
+    h2 {
+        text-align: center;
+    }
+
+    #main_img {
+        background-image: url("./assets/img/pingpong.png");
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        max-height: 30rem;
+    }
+
+    #main_img > img {
+        width: 100%;
+        max-height: 30rem;
+        object-fit: contain;
+        -webkit-backdrop-filter: blur(5px);
+        backdrop-filter: blur(5px);
+    }
+</style>
 </template>
 `
 let customGameplan;
